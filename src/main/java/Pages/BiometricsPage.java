bd2b0abfab0a9941d4bcc4cb75ba38bf8a4d92ef
devjosh0 <husseinamadu634@gmail.com>
2025-08-22 05:36:59 +0000

    update

--- src/main/java/Pages/BiometricsPage.java
+++ src/main/java/Pages/BiometricsPage.java
@@ -2,22 +2,34 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
 
 public class BiometricsPage extends BasePage {
 
-    private final By setupBiometrics = By.id("setup-biometrics");
+    private final By biometricsBtn = By.id("setup-biometrics");
     private final By accountInfo = By.id("user-account-info");
 
     public BiometricsPage(WebDriver driver) {
         super(driver);
     }
 
+    FluentWait<WebDriver> fluentWait1 = new FluentWait<>(driver)
+            .withTimeout(Duration.ofSeconds(180))
+            .pollingEvery(Duration.ofSeconds(1))
+            .ignoring(Exception.class);
+
     public void setupBiometrics() {
-        waitForVisible(setupBiometrics).click();
+        fluentWait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("setup-biometrics")));
+        driver.findElement(biometricsBtn).click();
     }
 
     public void goToAccountInfo() {
-        waitForVisible(accountInfo).click();
+        fluentWait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-account-info")));
+        driver.findElement(accountInfo).click();
     }
 }
 
